596806ee143b5d0f34864eb53ecc8086a6ed04dd
Johnathan Silva <johnathan.contato@gmail.com>
2026-09-15 10:20:17 -0300

    feat: add Android 3D launcher

---
+++ imperium-3d/android/src/main/java/com/imperiumvale/game3d/AndroidLauncher.java
@@ -0,0 +1,24 @@
+package com.imperiumvale.game3d;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.imperiumvale.core.ImperiumGame;
+
+public final class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
+        config.useAccelerometer = false;
+        config.useCompass = false;
+        config.useGyroscope = false;
+        config.numSamples = 2;
+        config.useGL30 = true;
+
+        initialize(new ImperiumGame(), config);
+    }
+}
